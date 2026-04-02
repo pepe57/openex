67408a1673f7d0895c019375243b58b351f58dc8
Guillaume <guillaume.paris@filigran.io>
2026-04-02 15:43:24 +0200

    [backend] chore: remove defunct Caldera injector references (#5340)

--- openaev-api/src/main/java/io/openaev/rest/injector/InjectorApi.java
+++ openaev-api/src/main/java/io/openaev/rest/injector/InjectorApi.java
@@ -160,45 +160,6 @@ public class InjectorApi extends RestBehavior {
     return injectorService.registerExternalInjector(input, file);
   }
 
-  @GetMapping(
-      value = "/api/implant/caldera/{platform}/{arch}",
-      produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-  @RBAC(skipRBAC = true)
-  public @ResponseBody byte[] getCalderaImplant(
-      @PathVariable String platform, @PathVariable String arch) throws IOException {
-    return getCalderaFile(platform, arch, null);
-  }
-
-  @GetMapping(
-      value = "/api/implant/caldera/{platform}/{arch}/{extension}",
-      produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-  @RBAC(skipRBAC = true)
-  public @ResponseBody byte[] getCalderaScript(
-      @PathVariable String platform, @PathVariable String arch, @PathVariable String extension)
-      throws IOException {
-    return getCalderaFile(platform, arch, extension);
-  }
-
-  private byte[] getCalderaFile(String platform, String arch, String extension) throws IOException {
-    if (!AVAILABLE_PLATFORMS.contains(platform)) {
-      throw new IllegalArgumentException("Platform invalid : " + platform);
-    }
-    if (!AVAILABLE_ARCHITECTURES.contains(arch)) {
-      throw new IllegalArgumentException("Architecture invalid : " + arch);
-    }
-
-    String resource =
-        "/implants/caldera/" + platform + "/" + arch + "/oaev-implant-caldera-" + platform;
-    if (extension != null) {
-      resource += "." + extension;
-    }
-    InputStream in = getClass().getResourceAsStream(resource);
-    if (in != null) {
-      return IOUtils.toByteArray(in);
-    }
-    return null;
-  }
-
   // Public API
   @GetMapping(
       value = "/api/implant/openaev/{platform}/{architecture}",
--- openaev-api/src/main/resources/implants/caldera/windows/x86_64/oaev-implant-caldera-windows.ps1
+++
@@ -1,2 +0,0 @@
-$server=$args[0]
-& "C:\Program Files (x86)\Filigran\OAEV Caldera\oaev-agent-caldera.exe" -server $server -group red
